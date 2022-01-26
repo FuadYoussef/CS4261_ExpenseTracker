1fe233229cb525deb503185fd40a944add2e98d4
Fuad Youssef <fuadmyoussef@gmail.com>
2022-01-26 01:04:32 -0500

    Initial commit

---
+++ app/src/main/java/com/mas/expensetracker/CreateAccountActivity.kt
@@ -0,0 +1,13 @@
+package com.mas.expensetracker
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class CreateAccountActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+    fun createAccount(view: View) {}
+}
