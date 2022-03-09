b4674a9322851e874615164ef07e62286eeb92bf
Fuad Youssef <fuadmyoussef@gmail.com>
2022-03-09 15:11:33 -0500

    Users now join group through a join code

--- app/src/main/java/com/mas/expensetracker/CreateGroupActivity.kt
+++ app/src/main/java/com/mas/expensetracker/CreateGroupActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
@@ -41,8 +43,7 @@ class CreateGroupActivity: AppCompatActivity() {
     fun createGroup(view: View) {
         var mAuth = FirebaseAuth.getInstance()
         var currentUser = mAuth.currentUser
-        val participantsET = findViewById<EditText>(R.id.participants_et)
-        var participants = participantsET.text.toString().split(',') as ArrayList<String>
+        var participants = ArrayList<String>()
         if (currentUser != null) {
             val groupName = findViewById<EditText>(R.id.group_name_et).text.toString()
             participants.add(currentUser.email.toString())
@@ -51,10 +52,41 @@ class CreateGroupActivity: AppCompatActivity() {
             databaseRef.child("Groups").child(key.toString()).child("participants").setValue(participants)
             databaseRef.child("Groups").child(key.toString()).child("groupName").setValue(groupName)
             databaseRef.child("Groups").child(key.toString()).child("groupDescription").setValue(groupDescription)
-
+            databaseRef.child("Groups").child(key.toString()).child("groupKey").setValue(key.toString())
             myGroups.add(key.toString())
             val gson = Gson()
             databaseRef.child("users").child(currentUser.uid).child("Groups").setValue(gson.toJson(myGroups))
+            var expensesIntent = Intent(this, GroupsActivity::class.java)
+            finish()
+            startActivity(expensesIntent)
+        }
+    }
+
+    fun joinGroup(view: View) {
+        var key = findViewById<EditText>(R.id.join_group_et).text.toString()
+        var participants = ArrayList<String>()
+        var mAuth = FirebaseAuth.getInstance()
+        var currentUser = mAuth.currentUser
+
+        databaseRef.child("Groups").child(key).child("participants").get().addOnSuccessListener {
+            val gson = Gson() //https://howtodoinjava.com/gson/gson-parse-json-array/
+            val arrayListType = object :
+                TypeToken<ArrayList<String>>() {}.type //https://www.bezkoder.com/kotlin-parse-json-gson/
+            if (currentUser != null) {
+                participants = gson.fromJson<ArrayList<String>>(it.value.toString(), arrayListType)
+                participants.add(currentUser.email.toString())
+                databaseRef.child("Groups").child(key).child("participants")
+                    .setValue(gson.toJson(participants))
+            }
+
+        }.addOnFailureListener {
+            Log.e("firebase", "Error getting data1", it)
+        }
+        if (currentUser != null) {
+            myGroups.add(key)
+            val gson = Gson()
+            databaseRef.child("users").child(currentUser.uid).child("Groups").setValue(gson.toJson(myGroups))
+
             var expensesIntent = Intent(this, GroupsActivity::class.java)
             finish()
             startActivity(expensesIntent)
